0993c0d4d88d5f13f8120674ba3993195f67a3a1
H DZ <2119921883@qq.com>
2022-10-21 17:16:29 +0800

    initial commit

---
+++ client/src/views/login/index.jsx
@@ -0,0 +1,9 @@
+import React, { memo } from "react";
+import styled from "styled-components";
+
+const LoginWrapper = styled.div``;
+const Login = memo(() => {
+  return <LoginWrapper></LoginWrapper>;
+});
+
+export default Login;
